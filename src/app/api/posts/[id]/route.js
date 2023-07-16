156b0584dbff15cfc5cbbaff99b51cc74bf1b020
j4rv1sgg <jrvs.inc@gmail.com>
2023-07-16 23:56:42 +0200

    added dashboard

--- src/app/api/posts/[id]/route.js
+++ src/app/api/posts/[id]/route.js
@@ -1,19 +1,31 @@
-import {NextResponse} from "next/server";
-import connect from "@/utils/db"
-import Post from "@/models/Post"
+import { NextResponse } from "next/server";
+import connect from "@/utils/db";
+import Post from "@/models/Post";
 
-export const GET = async (request, {params}) => {
+export const GET = async (request, { params }) => {
+  const { id } = params;
 
-  const {id} = params
-
-  try{
+  try {
     await connect();
 
-    const post = await Post.findById(id)
+    const post = await Post.findById(id);
 
-    return new NextResponse(JSON.stringify(post), { status: 200 })
-  }catch(err){
-    return new NextResponse("Data error", {status: 500})
+    return new NextResponse(JSON.stringify(post), { status: 200 });
+  } catch (err) {
+    return new NextResponse("Database Error", { status: 500 });
   }
+};
 
-}
+export const DELETE = async (request, { params }) => {
+  const { id } = params;
+
+  try {
+    await connect();
+
+    await Post.findByIdAndDelete(id);
+
+    return new NextResponse("Post has been deleted", { status: 200 });
+  } catch (err) {
+    return new NextResponse("Database Error", { status: 500 });
+  }
+};
